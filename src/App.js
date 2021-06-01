ed55ce4075e4b80c02a4c025277cc29c154bd498
Naim Zaman <egnaimur@gmail.com>
2021-06-01 23:18:31 +0600

    Data Maping & Pass Data Component Dynamic Way

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+	/*=====>>Fake Product Data [Start]<<=====*/
+	const products = [
+		{
+			name: 'Photoshop',
+			price: '$90.99'
+		},
+		{
+			name: 'Illustrator',
+			price: '$60.99'
+		},
+		{
+			name: 'PDF Reader',
+			price: '$6.99'
+		},
+		{
+			name: 'Premiere Element',
+			price: '$249.99'
+		},
+	]
+/*=====>>Fake Product Data [End]<<=====*/	
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<header className="App-header">
+			{
+				products.map(product =><Product product={product}></Product>)
+			}
+		</header>
     </div>
   );
-}
 
+  function Product(props){
+	  const productStyle={
+		  width: '200px',
+		  height: '200px',
+		  borderRadius: '7px',
+		  backgroundColor: 'lightgray',
+		  color:'#000',
+		  marginBottom:'10px'
+	  }
+	  return(
+		<div style={productStyle}>
+			<p>{props.product?.name}</p>
+			<p>{props.product?.price}</p>
+			<button>Bye Now</button>
+		</div>
+	  )
+  }
+}
 export default App;
